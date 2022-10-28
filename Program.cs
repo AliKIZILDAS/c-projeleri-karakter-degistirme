aa28fce5ba2bf98751f4fd9688a8d23ab07f03ec
Ali Kızıldaş <ali.kizildas@gmail.com>
2022-10-28 13:45:16 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s="Merhaba Hello Algoritma x";
+            string ilkKarakter="";
+            string yeniS="";
+
+            for (int i = 0; i <= s.Count(); i++)
+            {
+                if (i==0)
+                {
+                    ilkKarakter=s[i].ToString();
+                }
+                else if (i==s.Length)
+                {
+                     yeniS+=ilkKarakter;
+                } 
+                else if (s[i].ToString()==" ")
+                {
+                     yeniS+=ilkKarakter+" ";
+                }
+                else if (s[i-1].ToString()==" ")
+                {
+                     ilkKarakter=s[i].ToString();
+                }                                 
+                else
+                {
+                    yeniS+=s[i].ToString();
+                }                             
+            }             
+                      
+            Console.WriteLine(yeniS);
+        }
+    }
+}
